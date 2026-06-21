47bed1c6fc0123efa8fb8cec2caae6a8ef5a18e0
woojanyin <woojanyin@graduate.utm.my>
2026-06-21 19:25:13 +0800

    Update test scripts

--- tests/.ipynb_checkpoints/test_data_quality-checkpoint.py
+++ tests/.ipynb_checkpoints/test_data_quality-checkpoint.py
@@ -4,12 +4,3 @@ df = pd.read_csv("Bank_Personal_Loan_Modelling.csv")
 
 def test_no_missing_values():
     assert df.isnull().sum().sum() == 0
-
-def test_no_duplicates():
-    assert df.duplicated().sum() == 0
-
-def test_income_non_negative():
-    assert (df["Income"] >= 0).all()
-
-def test_age_range():
-    assert df["Age"].between(18, 100).all()
